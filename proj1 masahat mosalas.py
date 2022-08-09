566a6fceb9864373bfbe0841726cad9f6816d029
reza zaker tavallaei <110588050+rezazakertavallaei@users.noreply.github.com>
2022-08-09 10:54:51 +0430

    Update proj1 masahat mosalas.py

--- proj1 masahat mosalas.py
+++ proj1 masahat mosalas.py
@@ -1,7 +1,7 @@
 
 
 
-x=int(input('ghaedeh ra vard konid'))
-y=int(input('ertefaee  ra vard konid'))
+x=float(input('ghaedeh ra vard konid'))
+y=float(input('ertefaee  ra vard konid'))
 masahat=(x*y)/2
 print("masahat=",masahat)
